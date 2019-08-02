eed22d5db69704e389e2e33042a48488755c959a
Jamie Lowry <jamie.lowry@airbnb.com>
2019-08-02 16:29:29 -0700

    basic script to convert text

---
+++ scrabbletext.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from sys import argv
+
+def convert_string(string):
+
+    modified = []
+    for item in string:
+        if item == ' ':
+            substring = ':scrabble_blank:'
+        else:
+            substring = ':scrabble_{0}:'.format(item)
+
+        modified.append(substring)
+
+    rendered_string = ''.join(modified)
+
+    print(rendered_string)
+    return rendered_string
+
+
+if __name__ == '__main__':
+    convert_string(argv[1])
